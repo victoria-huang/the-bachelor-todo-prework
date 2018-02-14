e046291c26c080ddb5d2344fd1633c238a3733c3
victoria-huang <vctrah@gmail.com>
2018-02-14 15:08:21 -0500

    bachelor methods

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,65 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+    winner_name = nil
+    
+    data[season].each do |contestant|
+        
+        if contestant["status"] == "Winner"
+            winner_name = contestant["name"].split(" ")
+        end
+    end
+    
+    winner_name[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+      
+      contestants.each do |contestant|
+          if contestant["occupation"] == occupation
+              return contestant["name"]
+          end
+      end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  
+  data.each do |season, contestants|
+      
+      contestants.each do |contestant|
+            if contestant["hometown"] == hometown
+                count += 1
+            end
+      end
+  end
+  
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |season, contestants|
+        
+        contestants.each do |contestant|
+            if contestant["hometown"] == hometown
+                return contestant["occupation"]
+            end
+        end
+    end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_ages = 0
+  
+  data[season].each do |contestant|
+      total_ages += contestant["age"].to_f
+  end
+  
+  total_contestants = data[season].length
+  average = total_ages / total_contestants
+  average.round
 end
+
+
